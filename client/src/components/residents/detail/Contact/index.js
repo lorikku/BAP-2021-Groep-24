262721b94f0f152f315e4c2bb0dfff854e7c0c56
Arthur Robaeys <robaeys.arthur@icloud.com>
2021-02-03 20:22:28 +0100

    contact dropdown

--- client/src/components/residents/detail/Contact/index.js
+++ client/src/components/residents/detail/Contact/index.js
@@ -1,28 +1,40 @@
 import * as React from "react";
 import "./contact.css";
+import ContactDropdown from "../ContactDropdown";
 
 const Contact = ({ contact }) => {
   return (
-    <li className="contact-container">
-      <img
-        className="contact-pic"
-        alt="fototje van contactpersoon"
-        src="https://1tsip9tt643kufi0v3m1s4is-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/accessories-for-elderly-women.jpg"
-      ></img>
-      <div className="contact-info-wrapper">
-        <p className="contact-name">{contact.name}</p>
-        <div className={`contact-type${contact.matchedInterests.length > 0 ? '  contact-type--contact' : ''}`}>
-          <p className="contact-type-text">{contact.matchedInterests.length > 0 ? 'Match' : 'Vriend'}</p>
-        </div>
-      </div>
-      <div className="contact-info-btn">
+    <>
+      <li className="contact-container">
         <img
-          className="contact-info-btn-vector"
-          alt="chevron down icon"
-          src="/assets/img/chevron-down.svg"
+          className="contact-pic"
+          alt="fototje van contactpersoon"
+          src="https://1tsip9tt643kufi0v3m1s4is-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/accessories-for-elderly-women.jpg"
         ></img>
-      </div>
-    </li>
+        <div className="contact-info-wrapper">
+          <p className="contact-name">{contact.name}</p>
+          <div
+            className={`contact-type${
+              contact.matchedInterests.length > 0
+                ? "  contact-type--contact"
+                : ""
+            }`}
+          >
+            <p className="contact-type-text">
+              {contact.matchedInterests.length > 0 ? "Match" : "Vriend"}
+            </p>
+          </div>
+        </div>
+        <div className="contact-info-btn">
+          <img
+            className="contact-info-btn-vector"
+            alt="chevron down icon"
+            src="/assets/img/chevron-down.svg"
+          ></img>
+        </div>
+      </li>
+      <ContactDropdown />
+    </>
   );
 };
 
---
+++ client/src/components/residents/detail/ContactDropdown/index.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import "./contactdropdown.css";
+import Tag from "../../../interests/Tag";
+
+const Contact = () => {
+  return (
+    <li className="contact-dropdown">
+      <div className="contact-dropdown-grid">
+        <img
+          className="delete-contact-icon"
+          alt="vuilbakje"
+          src="/assets/img/trash-square.svg"
+        ></img>
+        <div className="dropdown-title-wrapper">
+          <p className="dropdown-title">Match gemaakt op:</p>
+          <p className="dropdown-subtitle">12 November 2017</p>
+        </div>
+      </div>
+      {/* alleen bij match --> */}
+      <div className="dropdown-tags-container">
+        <p className="dropdown-tags-title">Gematched op</p>
+        <ul className="dropdown-tags-list">
+          <Tag name={"Pop Smoke Woooo"} />
+        </ul>
+      </div>
+      {/* <--- */}
+    </li>
+  );
+};
+
+export default Contact;
