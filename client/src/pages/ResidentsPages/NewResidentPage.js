92462a1cd27e713ae9dae2b642fe6dfaa5088988
Arthur Robaeys <robaeys.arthur@icloud.com>
2021-02-05 23:16:54 +0100

    ghost component fixed

--- client/src/pages/ResidentsPages/NewResidentPage.js
+++ client/src/pages/ResidentsPages/NewResidentPage.js
@@ -1,26 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 
-import ResidentsFilter from '../../containers/residents/ResidentsFilter';
-import OverviewProfiles from '../../containers/residents/overview/OverviewProfiles';
-import NewResidentCreated from '../../containers/residents/new-resident/NewResidentCreated';
-import GoBack from '../../components/global/GoBack';
-import BannerHeader from '../../components/global/BannerHeader';
-// import NewResidentForm from '../../containers/residents/new-resident/NewResidentForm';
+import ResidentsFilter from "../../containers/residents/ResidentsFilter";
+import OverviewProfiles from "../../containers/residents/overview/OverviewProfiles";
+import NewResidentCreated from "../../containers/residents/new-resident/NewResidentCreated";
+import GoBack from "../../components/global/GoBack";
+import BannerHeader from "../../components/global/BannerHeader";
+import ResidentForm from "../../containers/residents/ResidentForm";
 
 const NewResidentPage = () => {
   return (
     <>
       <h2 className="visually-hidden">Nieuwe bewoner aanmaken</h2>
       <div className="new-resident-container fit-height">
-        <GoBack text={'Annuleren'} />
+        <GoBack text={"Annuleren"} />
         <BannerHeader
-          title={'Hallo!'}
+          title={"Hallo!"}
           subtext={
-            'Laten we samen een nieuwe bewoner in het platform toevoegen'
+            "Laten we samen een nieuwe bewoner in het platform toevoegen"
           }
           isNewResident
         />
-        {/* <NewResidentForm /> */}
+        <ResidentForm />
       </div>
     </>
   );
